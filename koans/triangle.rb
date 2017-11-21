33792f911e6fa00747ef6b97bbf52d5a117ee4ae
1627143 <1627143@stud.hs-mannheim.de>
2017-11-21 15:39:46 +0100

    183 Bahnfahrt

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -1,3 +1,4 @@
+
 # Triangle Project Code.
 
 # Triangle analyzes the lengths of the sides of a triangle
@@ -14,7 +15,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  [:equilateral, :isosceles, :scalene].fetch([a,b,c].uniq.size - 1)
 end
 
 # Error class used in part 2.  No need to change this code.
