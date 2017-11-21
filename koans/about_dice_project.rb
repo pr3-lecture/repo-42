3ef1f9b34aa669f233b47dc338e7fa510b3ce456
1627143 <1627143@stud.hs-mannheim.de>
2017-11-21 23:32:48 +0100

    212/282

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  def roll(x)
+  end
+
+  def values
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
